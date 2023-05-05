5d2f3d7ca24a1c25a796bc3c875daa6f966b99f4
kojirou <kojirouhtc@gmail.com>
2023-05-06 05:06:16 +0800

    safe codes

--- Sources/proxyworld-cli/UpdateGeodb.swift
+++ Sources/proxyworld-cli/UpdateGeodb.swift
@@ -46,8 +46,8 @@ struct UpdateGeodb: AsyncParsableCommand {
       print("Clash exit status:", try! process.wait())
     }
 
-    let stream = try FileStream.open(process.stdout!, mode: .read()).get()
-
+    let stream = try FileStream.open(process.stdout.unwrap("process's stdout is not set properly").duplicate(), mode: .read()).get()
+    defer { _ = stream.close() }
     try withUnsafeTemporaryAllocation(of: CChar.self, capacity: 4096) { buffer in
       strlen(buffer.baseAddress!)
       guard stream.getLine(into: buffer) else {
